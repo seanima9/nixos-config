66f9393e653062bcf386edadbe8430120ee8ee37
Sean Imani <seanimani@proton.me>
2026-09-15 00:43:03 +0100

    Fix issue with Electron apps not finding keyring.

--- home/programs/apps.nix
+++ home/programs/apps.nix
@@ -1,11 +1,16 @@
 { pkgs, ... }:
 {
-  programs.vscode.enable = true;
+  programs.vscode = {
+    enable = true;
+    argvSettings."password-store" = "gnome-libsecret";
+  };
   home.packages = with pkgs; [
     brave-origin
     trezor-suite
     pavucontrol
     seahorse
-    code-cursor
+    (code-cursor.override {
+      commandLineArgs = "--password-store=gnome-libsecret";
+    })
   ];
 }
